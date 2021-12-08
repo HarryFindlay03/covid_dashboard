dd45118c4c0b07e32607ae9d06488a4e26436e97
Harry Findlay <harryfindlay@outlook.com>
2021-12-08 12:38:20 +0000

    pylint is fun

--- covid_news_handling.py
+++ covid_news_handling.py
@@ -1,24 +1,31 @@
+"""Function that gets the news API request and updates the news."""
 import json
+import logging
 import requests
-from datetime import datetime
-from time_difference import time_to_go
 
-'''
 #Congiguring logging
 FORMAT = '%(levelname)s: %(asctime)s: %(message)s'
 logging.basicConfig(filename='program_log.log', format=FORMAT, level=logging.INFO)
-'''
 
-with open('config.json') as f:
+with open('config.json', 'r', encoding="utf8") as f:
     data = json.load(f)
 
 API_KEY = '&apiKey=' + data["keys"]["news"]
 
 queue = []
 
-def news_API_request(covid_terms="Covid COVID-19 coronavirus"):
-    #logging.info('PROGRAM LOG: NEWS UPDATE')
-    print("NEWS UPDATE COMMENCING")
+def news_API_request(covid_terms="Covid COVID-19 coronavirus") -> list:
+    """newsapi request that returns a list of articles got from the constructed URL
+    in the function
+
+    Args:
+        covid_terms (str, optional): The keyword terms that are added into the URL.
+        Defaults to "Covid COVID-19 coronavirus".
+
+    Returns:
+        list: List of articles returned from the request
+    """
+    logging.info('NEWS UPDATE')
     articles = []
     keywords = covid_terms.split()
     query = ''
@@ -28,10 +35,10 @@ def news_API_request(covid_terms="Covid COVID-19 coronavirus"):
     base_url = 'https://newsapi.org/v2/everything?'
     final_url = base_url + query + 'language=en' + API_KEY
 
-    r = requests.get(final_url)
-    data = r.json()
+    req = requests.get(final_url)
+    articles_json = req.json()
 
-    for article in data["articles"]:
+    for article in articles_json["articles"]:
         temp = {}
         temp["title"] = article["title"]
         temp["content"] = article["description"]
@@ -40,9 +47,22 @@ def news_API_request(covid_terms="Covid COVID-19 coronavirus"):
 
     return articles
 
-def update_news(testing='false'):
-    if testing == 'false':
-        articles = news_API_request()
-        return articles
-    elif testing == 'test':
-        pass
+def update_news(testing='false') -> list:
+    """Function that re runs the news API request
+
+    Args:
+        testing (str, optional): check whether to run the API request or not.
+        Defaults to 'false'.
+
+    Returns:
+        list: List of news articles
+    """
+    if testing == 'test':
+        test_article = {}
+        test_articles = []
+        test_article["title"] = "COOL NEWS"
+        test_article["content"] = "COVID IS BAD"
+        test_articles.append(test_article)
+        return test_article
+    articles = news_API_request()
+    return articles
